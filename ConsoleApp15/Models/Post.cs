432d2ab2aaa6c89ae64c0a37187e760b9d963e32
muradmecidov <muradmecidov1499@gmail.com>
2023-05-03 12:42:56 +0400

    task167

---
+++ ConsoleApp15/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15.Models
+{
+    public class Post
+    {
+        //string urls = "https://jsonplaceholder.typicode.com/posts";
+
+        //public void GetHttpContent(string url)
+        //{
+        //    HttpClient httpClient = new HttpClient();
+        //foreach (var itme in url)
+        //    {
+        //        var result = httpClient.GetStringAsync(itme);
+        //         list.Add(result);
+        //    }
+        //}
+
+
+
+
+
+
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,70 @@
+﻿using ConsoleApp15.Models;
+using System.Text.Json;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Task 1
+            //string path = @"C:\Users\murad\Desktop\Models\Data";
+            string path1 = @"C:\Users\murad\Desktop\Models\Data\jsonData.json";
+
+            //Directory.CreateDirectory(path);
+
+
+
+            //if (!File.Exists(path1)) 
+            //{
+            //    File.Create(path1);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Movcuddur");
+            //}
+            //string text = "";
+
+            //using (StreamReader stream = new StreamReader(path1))
+            //{
+            //    text = stream.ReadToEnd();
+            //}
+            //using (StreamWriter stream = new StreamWriter(path1))
+            //{
+            //    stream.WriteLine(text);
+            //    stream.WriteLine("salam dostlar");
+
+            //} 
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Task2 
+
+
+
+            string jsonurl = "https://jsonplaceholder.typicode.com/posts";
+
+            HttpClient url = new HttpClient();
+            string json = url.GetStringAsync(jsonurl).Result;
+            List<Post> posts = JsonSerializer.Deserialize<List<Post>>(json);
+
+            Console.WriteLine(posts[0].Body);
+
+
+
+        }
+    }
+}
